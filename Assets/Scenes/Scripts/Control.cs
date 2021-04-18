8b0c897fa4dc6d5f98a7c89e3ffbfd8a0f07f611
L Yu <32178113+ls-yu@users.noreply.github.com>
2021-04-18 01:23:47 -0400

    init

---
+++ Assets/Scenes/Scripts/Control.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject orb1;
+    private int spawnAmt = 10;
+    private float spawnDist = 2f;
+    private float spawnRange = 1f;
+    private float r_low;
+    private float r_high;
+    private float b_low;
+    private float b_high;
+    private float g_low;
+    private float g_high;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color32 cam_color = new Color32((byte)Random.Range(r_low,r_high), (byte)Random.Range(g_low,g_high), (byte)Random.Range(b_low,b_high), (byte)255);
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        cam.backgroundColor = cam_color;
+        r_low = Random.Range(0,235);
+        r_high = r_low + 20;
+        b_low = Random.Range(0,235);
+        b_high = r_low + 20;
+        g_low = Random.Range(0,235);
+        g_high = r_low + 20;
+
+        Debug.Log(r_low + "-" + r_high);
+        Debug.Log(g_low + "-" + g_high);
+        Debug.Log(b_low + "-" + b_high);
+        spawnAmt = Random.Range(8,15);
+        for (int i = 0; i < spawnAmt; i++)
+        {
+            spawnDist = Random.Range(-5,5);
+            spawnRange = Random.Range(-4,4);
+            Color32 obj_color = new Color32((byte)Random.Range(r_low,r_high), (byte)Random.Range(g_low,g_high), (byte)Random.Range(b_low,b_high), (byte)255);
+            GameObject instance = Instantiate(orb1, new Vector3(spawnDist, spawnRange, 0), Quaternion.identity) as GameObject;
+            instance.GetComponent<SpriteRenderer>().color = obj_color;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/OrbScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbScript : MonoBehaviour
+{
+    public GameObject orb1;
+    private bool selected = false;
+    public float red;
+    public float green;
+    public float blue;
+    //public Transform trans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        red = (float)GetComponent<SpriteRenderer>().color.r * 255;
+        green = (float)GetComponent<SpriteRenderer>().color.g * 255;
+        blue = (float)GetComponent<SpriteRenderer>().color.b * 255;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected){
+            gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,Mathf.Abs(Camera.main.transform.position.z-transform.position.z)));
+        }
+        
+    }
+
+    public float getRed(){
+        return red;
+    }
+
+    public float getBlue(){
+        return blue;
+    }
+
+    public float getGreen(){
+        return green;
+    }
+
+    public void OnMouseOver(){
+        if(!selected && Input.GetMouseButtonDown(0)){
+            selected = true;
+        }
+        else if (selected && Input.GetMouseButtonDown(0)){
+            selected = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll){
+        Debug.Log("collided");
+        Debug.Log("curr size;" + coll.bounds.size.x);
+        float new_size = coll.bounds.size.x + gameObject.GetComponent<Collider2D>().bounds.size.x;
+        Debug.Log("new size;" + new_size);
+        GameObject other = coll.gameObject;
+        float new_red = (red + (float)other.GetComponent<SpriteRenderer>().color.r * 255)/2;
+        float new_green = (green + (float)other.GetComponent<SpriteRenderer>().color.g * 255)/2;
+        float new_blue = (blue + (float)other.GetComponent<SpriteRenderer>().color.b * 255)/2;
+        Color32 obj_color = new Color32((byte)new_red, (byte)new_green, (byte)new_blue, (byte)255);
+        GameObject instance = Instantiate(orb1, transform.position, Quaternion.identity) as GameObject;
+        instance.GetComponent<SpriteRenderer>().color = obj_color;
+        instance.transform.localScale = new Vector3(1+new_size, 1+new_size, 1);
+        Destroy(other);
+        Destroy(gameObject);
+    }
+}
